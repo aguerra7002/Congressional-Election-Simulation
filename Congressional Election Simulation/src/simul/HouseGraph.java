f2a63ab079e922d215cadaa02c049f6ab74fa94a
Alex Guerra <aguerra7002@stu.d214.org>
2016-12-31 00:46:00 -0800

    UI Improvements

--- Congressional Election Simulation/src/simul/HouseGraph.java
+++ Congressional Election Simulation/src/simul/HouseGraph.java
@@ -21,6 +21,7 @@ public class HouseGraph extends JPanel {
 		ArrayList<District> d = dm.getDistricts();
 		int dem = 0; 
 		int rep = 0;
+		g.drawString("House of Representatives", 4, 13);
 		for (int i = 0; i < d.size(); i++) {
 			
 			if (d.get(i).isDemocrat){
@@ -33,9 +34,10 @@ public class HouseGraph extends JPanel {
 			int width = getWidth() - 12;
 			int x = (i % width)* 20 % width;
 			int y = ((i*20)/width) * 20;
-			g.fillOval(x + 5, y + 5, 20, 20);
+			g.fillOval(x + 5, y + 17, 20, 20);
 			//System.out.println(d.get(i).pvi + "\t" + d.get(i).gerrymanderedRating);
 		}
+		g.setColor(Color.BLACK);
 		g.drawString("Democrat Seats: " + dem + "  Republican Seats: " + rep, 5, getHeight() - 20);
 		System.out.println(dm.getYear() + "\t" + dem + "\t" + rep + "\t" + dm.getYear() + "\t" + dm.getK() + "\t" + dm.getYear() + "\t" + dm.getSwing() + "\t" + dm.getYear() + "\t" + dm.getNumSwitched());
 		
--- Congressional Election Simulation/src/simul/SwingDistrictGraph.java
+++ Congressional Election Simulation/src/simul/SwingDistrictGraph.java
@@ -1,6 +1,7 @@
 package simul;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
@@ -46,22 +47,49 @@ public class SwingDistrictGraph extends JPanel {
 			}
 		}
 		int bottom = getHeight() - 40;
-		g.drawLine(20, 20, 20, bottom);
-		g.drawLine(20, bottom, getWidth() - 10, bottom);
+		g.drawString("Frequency of Districts in Each PVI Category", 100, 20);
+		g.drawString("0", 10, bottom + 4);
+		g.drawLine(18, bottom - 120, 22, bottom - 120); // 40 districts
+		g.drawString("40", 2, bottom - 116);
+		g.drawLine(18, bottom - 240, 22, bottom - 240); // 40 districts
+		g.drawString("80", 2, bottom - 236);
+		g.drawLine(20, bottom - 360, 22, bottom - 360); // 40 districts
+		g.drawString("120", 0, bottom - 356);
+		g.drawLine(22, 20, 22, bottom);
+		g.drawLine(18, bottom, getWidth() - 10, bottom);
 		g.setColor(new Color(0,0,100));
 		g.fillRect(30, bottom - (demSolid * 3), 20, demSolid * 3);
+		g.drawString(demSolid + "", 30, bottom - (demSolid * 3) - 2);
 		g.setColor(new Color(0,0,175));
 		g.fillRect(95, bottom - (demLikely * 3), 20, demLikely * 3);
+		g.drawString(demLikely + "", 95, bottom - (demLikely * 3) - 2);
 		g.setColor(Color.BLUE);
 		g.fillRect(160, bottom - (demLean * 3), 20, demLean * 3);
+		g.drawString(demLean + "", 160, bottom - (demLean * 3) - 2);
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(225, bottom - (swing * 3), 20, swing * 3);
+		g.drawString(swing + "", 225, bottom - (swing * 3) - 2);
 		g.setColor(Color.RED);
 		g.fillRect(290, bottom - (repLean * 3), 20, repLean * 3);
+		g.drawString(repLean + "", 290, bottom - (repLean * 3) - 2);
 		g.setColor(new Color(175, 0, 0));
 		g.fillRect(355, bottom - (repLikely * 3), 20, repLikely * 3);
+		g.drawString(repLikely + "", 355, bottom - (repLikely * 3) - 2);
 		g.setColor(new Color(100, 0, 0));
 		g.fillRect(420, bottom - (repSolid * 3), 20, repSolid * 3);
+		g.drawString(repSolid + "", 420, bottom - (repSolid * 3) - 2);
+		g.setColor(Color.BLACK);
+		g.drawString("Solid", 22, bottom + 12);
+		g.drawString("Likely", 89, bottom + 12);
+		g.drawString("Lean", 154, bottom + 12);
+		g.drawString("Swing", 217, bottom + 12);
+		g.drawString("Lean", 284, bottom + 12);
+		g.drawString("Likely", 349, bottom + 12);
+		g.drawString("Solid", 412, bottom + 12);
+		g.drawString("Democrat <", 22, bottom + 32);
+		g.drawLine(82, bottom + 27 , 383, bottom + 27);
+		g.drawString("> Republican", 380, bottom + 32);
+		
 		dm.setSwing(swing);
 	}
 
